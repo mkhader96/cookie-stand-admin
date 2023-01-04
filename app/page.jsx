61f3116e3ea93cfe9bb625eaf8cb7e42865a7cd6
mkhader96 <mkhader996@gmail.com>
2023-01-04 12:22:33 +0000

    Version2

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Inter } from '@next/font/google'
-import Form from './components/From';
+import Form from './components/Form';
 
 const inter = Inter({ subsets: ['latin'] })
 
